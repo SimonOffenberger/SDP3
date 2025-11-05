d00afb2428ae9eb9aac820483bb70132deda7293
vogelhubersimon <113990284+vogelhubersimon@users.noreply.github.com>
2025-11-05 13:24:28 +0100

    AHHH

---
+++ Uebung4/SymbolParser/Object.h
@@ -0,0 +1,29 @@
+/*****************************************************************//**
+ * \file   Object.hpp
+ * \brief  common ancestor for all objects
+ *
+ * \author Simon
+ * \date   November 2025
+ *********************************************************************/
+#ifndef OBJECT_HPP
+#define OBJECT_HPP
+
+#include <string>
+
+class Object {
+public:
+
+	// Exceptions constants
+	inline static const std::string ERROR_BAD_OSTREAM = "ERROR: Provided Ostream is bad";
+	inline static const std::string ERROR_FAIL_WRITE = "ERROR: Fail to write on provided Ostream";
+	inline static const std::string ERROR_NULLPTR = "ERROR: Passed in Nullptr!";
+
+	// once virtual always virtual
+	virtual ~Object() = default;
+
+
+protected:
+	Object() = default;
+};
+
+#endif // !OBJECT_HPP
